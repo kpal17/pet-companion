5946d2aee049573c5e584aa3c8e6f40a29556f76
BartoszMordarski <bartosz.mordarski@gmail.com>
2026-06-11 22:44:56 +0200

    working google analytics and hotjar

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyCXDzjRfzgs5wHL2X854vJD7BCPeCo5BqE",
@@ -9,8 +9,9 @@ const firebaseConfig = {
   storageBucket: "pet-companion-9335f.firebasestorage.app",
   messagingSenderId: "71690345105",
   appId: "1:71690345105:web:5ef3b5207886b7296bfd30",
-  measurementId: "G-9PPC1762LE"
+  measurementId: "G-9PPC1762LE",
 };
 
 const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
